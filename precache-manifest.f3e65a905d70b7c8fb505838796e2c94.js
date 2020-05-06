299055c91d2d8deef6c6dc2a3e9828c3432dad10
Roy <royxuhan@gmail.com>
2020-05-05 23:47:07 -0400

    Updates

---
+++ precache-manifest.f3e65a905d70b7c8fb505838796e2c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f132ce04bdccb0daff1c180d0d5507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83a48acb988a9696281",
+    "url": "/static/css/2.d4afeea5.chunk.css"
+  },
+  {
+    "revision": "bb5618db83d14af4c076",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "c83a48acb988a9696281",
+    "url": "/static/js/2.47b62d7a.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/static/js/2.47b62d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5618db83d14af4c076",
+    "url": "/static/js/main.3cb8804d.chunk.js"
+  },
+  {
+    "revision": "8cb7b4204fa7cc4f813e",
+    "url": "/static/js/runtime-main.b8ac2b8e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.349590bcb466ef7853dd450a65727a88.js"
+  "/precache-manifest.f3e65a905d70b7c8fb505838796e2c94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
